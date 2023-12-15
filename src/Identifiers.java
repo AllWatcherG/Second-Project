c988d483be571a23c0f8805f542634a13d565ccc
AllWatcherG <92951041+AllWatcherG@users.noreply.github.com>
2023-12-15 08:35:09 -0500

    Identifiers pt 1

---
+++ src/Identifiers.java
@@ -0,0 +1,7 @@
+//identifiers
+
+public class Identifiers { //class name
+    public static void main(String[]  args){ //method name main(), predefined class name String[], argument name args
+       int x = 100; //integer x
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 public class Main {
 
-    public static void main(String[] args){ //main method this is the starting point
+    public static void main(String[] args){ //main method this is the starting point of this class
         System.out.println("Hello world");
+        int dob;
+        dob = 21;
+        System.out.println(dob);
+        System.out.println("Hello World");
+        System.out.println("Hello Java");
     }
 }
